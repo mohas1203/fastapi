a504df08ee5881cfaf2bf61c1f9c28dbef5b8510
Shaheed <shaheedPCad@gmail.com>
2021-12-27 17:32:44 -0500

    created new test conf

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,33 +1,33 @@
-from pydantic.main import create_model
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from app.database import get_db, Base
-from app.config import settings
-import pytest
-from app.main import app
 from fastapi.testclient import TestClient
-from app import models
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from app.main import app
+
+from app.config import settings
+from app.database import get_db
+from app.database import Base
 from app.oauth2 import create_access_token
+from app import models
+from alembic import command
 
-# Setting up test database
-SQLALCHEMY_DATABASE_URL_TEST = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL_TEST)
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:password123@localhost:5432/fastapi_test'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
+
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 TestingSessionLocal = sessionmaker(
     autocommit=False, autoflush=False, bind=engine)
 
-Base.metadata.create_all(bind=engine)
 
-
-# Fixture to run setup before tests
 @pytest.fixture()
 def session():
-
+    print("my session fixture ran")
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
-
     db = TestingSessionLocal()
     try:
         yield db
@@ -38,44 +38,46 @@ def session():
 @pytest.fixture()
 def client(session):
     def override_get_db():
+
         try:
             yield session
         finally:
             session.close()
-
     app.dependency_overrides[get_db] = override_get_db
     yield TestClient(app)
 
 
-# Fixture for test user
 @pytest.fixture
 def test_user2(client):
-    user_data = {"email": "test2@gmail.com", "password": "test123"}
+    user_data = {"email": "sanjeev123@gmail.com",
+                 "password": "password123"}
     res = client.post("/users/", json=user_data)
+
     assert res.status_code == 202
+
     new_user = res.json()
-    new_user['password'] = user_data["password"]
+    new_user['password'] = user_data['password']
     return new_user
 
 
-# Fixture for another test user
 @pytest.fixture
 def test_user(client):
-    user_data = {"email": "test@gmail.com", "password": "test123"}
+    user_data = {"email": "sanjeev@gmail.com",
+                 "password": "password123"}
     res = client.post("/users/", json=user_data)
+
     assert res.status_code == 202
+
     new_user = res.json()
-    new_user['password'] = user_data["password"]
+    new_user['password'] = user_data['password']
     return new_user
 
 
-# Fixture to create token
 @pytest.fixture
 def token(test_user):
-    return create_access_token({"user_id": test_user["id"]})
+    return create_access_token({"user_id": test_user['id']})
 
 
-# Fixture to use for authenticated requests
 @pytest.fixture
 def authorized_client(client, token):
     client.headers = {
@@ -89,40 +91,34 @@ def authorized_client(client, token):
 @pytest.fixture
 def test_posts(test_user, session, test_user2):
     posts_data = [{
-        "title": "First test post",
-        "content": "First test content",
+        "title": "first title",
+        "content": "first content",
         "owner_id": test_user['id']
     }, {
-        "title": "Second test post",
-        "content": "Second test content",
+        "title": "2nd title",
+        "content": "2nd content",
+        "owner_id": test_user['id']
+    },
+        {
+        "title": "3rd title",
+        "content": "3rd content",
         "owner_id": test_user['id']
     }, {
-        "title": "THird test post",
-        "content": "Third test content",
-        "owner_id": test_user['id']
-    }, {
-        "title": "Fourth test post",
-        "content": "Fourth test content",
+        "title": "3rd title",
+        "content": "3rd content",
         "owner_id": test_user2['id']
-    }, ]
+    }]
 
     def create_post_model(post):
         return models.Post(**post)
 
     post_map = map(create_post_model, posts_data)
+    posts = list(post_map)
 
-    posts_list = list(post_map)
-    print(posts_list)
-    session.add_all(posts_list)
-
+    session.add_all(posts)
+    # session.add_all([models.Post(title="first title", content="first content", owner_id=test_user['id']),
+    #                 models.Post(title="2nd title", content="2nd content", owner_id=test_user['id']), models.Post(title="3rd title", content="3rd content", owner_id=test_user['id'])])
     session.commit()
 
     posts = session.query(models.Post).all()
     return posts
-
-
-@pytest.fixture()
-def test_vote(test_posts, session, test_user):
-    new_vote = models.Vote(post_id=test_posts[3].id, user_id=test_user['id'])
-    session.add(new_vote)
-    session.commit()
