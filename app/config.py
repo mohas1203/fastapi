d43ff5480c9f4266aeab5713271b4aad140f1345
Shaheed <shaheedPCad@gmail.com>
2021-12-27 17:37:28 -0500

    made changes to config file

--- app/config.py
+++ app/config.py
@@ -1,20 +1,18 @@
 from pydantic import BaseSettings
 
 
-# Application settings
 class Settings(BaseSettings):
     database_hostname: str
     database_port: str
     database_password: str
-    database_username: str
     database_name: str
+    database_username: str
     secret_key: str
-    signing_algorithm: str
-    access_token_expire_min: int
+    algorithm: str
+    access_token_expire_minutes: int
 
     class Config:
         env_file = ".env"
 
 
-# Creating instance of settings class
 settings = Settings()
